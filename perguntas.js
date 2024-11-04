9605938766f2b34f79f0119c26f102eb00f2829d
Grazigama24 <00001219316477SP@aluno.educacao.sp.gov.br>
2024-11-04 17:52:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criCarta (
+  'Programaçao',
+  'O que é Python?',
+  'O JavaScript e uma linguagem de programaçao'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    ' A capital da França e Paris'
+)    
+   
+criaCartao (
+    'programacao',
+    ' o que e uma funçao?',
+    ' uma funçao e uum bloco de codigo que executa alguma tarefa',
+)
+
+criaCartao (
+'Lingua inglesa',
+'como se diz oi em ingles?',
+'oi em ingles e HI (RAI)'
+)
+
+
+
+
+
+
